efcb8cbc0e051f5798b1e38d6399cafaf392a895
highlander-dantas-snk <highlander.dantas@sankhya.com.br>
2026-04-13 21:27:52 -0300

    Atualização nivel 11

--- .03-iac-na-aws/03.25-security-groups/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sg_bastion_id" {
-  description = "ID do Security Group do Bastion"
-  value       = aws_security_group.bastion.id
-}
-
-output "sg_private_id" {
-  description = "ID do Security Group das instâncias privadas"
-  value       = aws_security_group.private.id
-}
